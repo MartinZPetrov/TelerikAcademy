1d4c47ca1abbc0adda858dde8ed2ea3781611fe4
MartinZPetrov <martinpetrov8907@gmail.com>
2015-01-08 23:44:01 +0200

    Intro

---
+++ 14. AgeAfter10Years/AgeAfter10Years.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.AgeAfter10Years
+{
+    class AgeAfter10Years
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter current age:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Age after 10 years will be \n{0}: ", age+10);
+        }
+    }
+}
---
+++ SquareRoot/SquareRoot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Problem 8. Square Root
+
+//Create a console application that calculates and prints the square root of the number 12345.
+//Find in Internet “how to calculate square root in C#”.
+namespace SquareRoot
+{
+    class SquareRoot
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(12345)); 
+        }
+    }
+}
