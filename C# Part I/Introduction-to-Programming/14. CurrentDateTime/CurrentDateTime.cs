fb53d785dd8c25cf465e68cb7b1bfff7ddc86c1f
MartinZPetrov <martinpetrov8907@gmail.com>
2015-01-13 22:12:10 +0200

    Updated project

---
+++ C# Part I/Introduction-to-Programming/14. CurrentDateTime/CurrentDateTime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Problem 14.* Current Date and Time
+//Create a console application that prints the current date and time. Find out how in Internet.
+namespace _14.CurrentDateTime
+{
+    class CurrentDateTime
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
---
+++ C# Part I/Introduction-to-Programming/15. AgeAfter10Years/AgeAfter10Years.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Problem 15.* Age after 10 Years
+//Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
+
+namespace _15.AgeAfter10Years
+{
+    class AgeAfter10Years
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter current age:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Age after 10 years will be \n{0}: ", age+10);
+        }
+    }
+}
